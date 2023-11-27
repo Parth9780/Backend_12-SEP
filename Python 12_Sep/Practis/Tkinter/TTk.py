7b8928ac6baa28258a6f0b9abdefebe04b7ad123
Parth9780 <pp7810559@gmail.com>
2023-11-27 18:22:20 +0530

    27-11-23

--- Python 12_Sep/Practis/Tkinter/TTk.py
+++ Python 12_Sep/Practis/Tkinter/TTk.py
@@ -1,5 +1,6 @@
 # Import the required libraries
 from tkinter import *
+from tkinter import messagebox 
 
 # Create an instance of tkinter frame or window
 win=Tk()
@@ -12,40 +13,52 @@ def cal_sum():
    t2=int(b.get())
    sum=t1+t2
    label.config(text=sum)
+   # messagebox.showinfo(f("Addition","{t1}+{t2}={sum}"))
 
 def sub_sum():
    t1=int(a.get())
    t2=int(b.get())
    sum=t1-t2
    label.config(text=sum)
+   # messagebox.showinfo("Subtriction",f("{t1}-{t2}={sum}"))
+
 
 def mul_sum():
    t1=int(a.get())
    t2=int(b.get())
    sum=t1*t2
    label.config(text=sum)
+   # messagebox.showinfo("multiplication",f("{t1}*{t2}={sum}"))
+
 
 def div_sum():
    t1=int(a.get())
    t2=int(b.get())
    sum=t1/t2
    label.config(text=sum)
+   # messagebox.showinfo("Division",f("{t1}/{t2}={sum}"))
+
 
 # Create an Entry widget
-Label(win, text="Enter First Number", font=('Calibri 10')).pack()
-a=Entry(win, width=35)
+t1=Label(win, text="Enter First Number", font=('Calibri 20'))
+t1.pack()
+a=Entry(win, font='Calibri 15', width=35)
 a.pack()
-Label(win, text="Enter Second Number", font=('Calibri 10')).pack()
-b=Entry(win, width=35)
+t2=Label(win, text="Enter Second Number", font=('Calibri 20'))
+t2.pack()
+b=Entry(win, font='Calibri 15', width=35)
 b.pack()
 
-label=Label(win, text="Total Sum : ", font=('Calibri 15'))
+label=Label(win, text="Total Sum : ", font=('Calibri 20'))
 label.pack(pady=20)
 
-Button(win, text="Calculate Sum", command=cal_sum).pack()
-Button(win, text="Calculate Sub", command=sub_sum).pack()
-Button(win, text="Calculate mul", command=mul_sum).pack()
-Button(win, text="Calculate div", command=div_sum).pack()
+# Button(win, text="Calculate Sum", command=cal_sum).pack()
+# Button(win, text="Calculate Sub", command=sub_sum).pack()
+# Button(win, text="Calculate mul", command=mul_sum).pack()
+# Button(win, text="Calculate div", command=div_sum).pack()
 
-
+Button(win,text="Calculate Sum", font='Calibri 15', command=cal_sum).place(x=200,y=200)
+Button(win,text="Calculate Sub", font='Calibri 15', command=sub_sum).place(x=350,y=200)
+Button(win,text="Calculate mul", font='Calibri 15', command=mul_sum).place(x=200,y=270)
+Button(win,text="Calculate div", font='Calibri 15', command=div_sum).place(x=350,y=270)
 win.mainloop()
